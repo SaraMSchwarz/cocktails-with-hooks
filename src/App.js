24eb9ba3e860580e56617a330ea46121504fca69
github username <saramschwarz@gmail.com>
2021-04-07 18:45:54 -0400

    input onChange and value edit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const [cocktails, setCocktails] = React.useState('');
   return (
     <div className="App">
       <h1>Find a cocktail recipe</h1>
-      <input placeholder="write here" onChange={(e) => console.log(e.target.value)} value="" />
+      <input
+        placeholder="write here"
+        onChange={(e) => setCocktails(e.target.value)}
+        value="cocktails"
+      />
       <button type="submit">Search</button>
     </div>
   );
