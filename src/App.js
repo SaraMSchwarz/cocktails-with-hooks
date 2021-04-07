59110d20842d2c7fc10f811328285d4a4324da81
github username <saramschwarz@gmail.com>
2021-04-07 18:29:32 -0400

    app skeleton with search bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Find a cocktail recipe</h1>
+      <input placeholder="write here" onChange={(e) => console.log(e.target.value)} value="" />
+      <button type="submit">Search</button>
     </div>
   );
 }
